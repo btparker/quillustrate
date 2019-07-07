f096375a59a758b622ecaccdd4b8a923ad36f8ad
Tyler Parker <btylerparker@gmail.com>
2019-07-07 13:24:44 -0400

    Start of structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Quillustrate',
+    version='0.1',
+    description='Means of using Quill in various 3D pipelines',
+    author='Tyler Parker',
+    author_email='btylerparker@gmail.com',
+)
